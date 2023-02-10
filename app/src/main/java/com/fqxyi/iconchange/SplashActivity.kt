d0b746abf94e268b2ed35082256e95a6961d8d0e
沈彬峰 <shen.binfeng@yalla.live>
2023-02-10 10:04:41 +0800

    @feat update readme

--- app/src/main/java/com/fqxyi/iconchange/SplashActivity.kt
+++ app/src/main/java/com/fqxyi/iconchange/SplashActivity.kt
@@ -12,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
-    application.registerActivityLifecycleCallbacks(monitor)
+//    application.registerActivityLifecycleCallbacks(monitor)
     Handler().postDelayed({
       startActivity(Intent(this, MainActivity::class.java))
     }, 1000)
@@ -20,6 +20,6 @@ class SplashActivity : AppCompatActivity() {
 
   override fun onDestroy() {
     super.onDestroy()
-    application.unregisterActivityLifecycleCallbacks(monitor)
+//    application.unregisterActivityLifecycleCallbacks(monitor)
   }
 }
